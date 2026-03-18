7443a5ba70a83727bf6d2fef4b43b451d3e94dc6
Nguyễn Tiến Vương <vuong20031591@gmail.com>
2026-03-18 21:10:28 +0700

    Khoi tao project springboot

---
+++ src/main/java/com/florastore/web_ban_hoa/WebBanHoaApplication.java
@@ -0,0 +1,13 @@
+package com.florastore.web_ban_hoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBanHoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBanHoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/florastore/web_ban_hoa/WebBanHoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.florastore.web_ban_hoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBanHoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
